4777d5714de6006dd80542065ec18a2752195344
Robin R <44073590+Robinrrr10@users.noreply.github.com>
2024-11-26 00:14:01 +0530

    Add files via upload

---
+++ Tutorial2Classes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleC_Project
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
+
+    public class Tutorial2Classes
+    {
+        static void Main2(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "John";
+            person.Introduce("Ram");
+        }
+    }
+}
---
+++ Tutorial2Classes2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleC_Project
+{
+    public class Person2
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person2 setPer(string n)
+        {
+            var per = new Person2();
+            per.Name = n;
+            return per;
+        }
+    }
+
+    public class Tutorial2Classes2
+    {
+        static void Main(string[] args)
+        {
+            Person2 person = Person2.setPer("Job");
+            person.Introduce("Ra");
+        }
+    }
+}
