ea3cd6e2da123a5d2c462d09f951ebc64593aaf4
Robin R <44073590+Robinrrr10@users.noreply.github.com>
2024-11-26 09:28:25 +0530

    adding constructor files

---
+++ Tutorial3Constructor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleC_Project
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+
+        public Student() 
+        {
+            
+        }
+
+        public Student(int id)
+        {
+            this.Id = id;
+        }
+
+        public Student(int id, string name) {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+
+    public class MyMainClass
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Id = 23;
+            student.Name = "Babu";
+            Console.WriteLine(student.Id);
+            Console.WriteLine(student.Name);
+
+            Student student2 = new Student(78, "Ram");
+            Console.WriteLine(student2.Id);
+            Console.WriteLine(student2.Name);
+        }
+    }
+}
---
+++ Tutorial3Constructor2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleC_Project
+{
+    class Student2
+    {
+        public int Id;
+        public string Name;
+        public List<Exam> exams;
+
+        public Student2() 
+        {
+            exams = new List<Exam>();
+        }
+
+        public Student2(int id)
+            : this() //This will call above constructor first
+        {
+            this.Id = id;
+        }
+
+        public Student2(int id, string name) 
+            : this(id)  //This will call above constructor first
+        {
+            this.Name = name;
+        }
+    }
+
+    public class MyMainClass2
+    {
+        public static void Main(string[] args)
+        {
+          var student = new Student2(34, "Raju");
+          Console.WriteLine(student.Id);
+          Console.WriteLine(student.Name);
+        }
+    }
+}
